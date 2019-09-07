c03a3ae971863d39b9f2020d6bb38427ca4586b3
Katarzyna Siwiec <katasiw97@gmail.com>
2019-09-07 20:50:15 +0200

    created all choices variables

---
+++ utils/user_options.py
@@ -0,0 +1,31 @@
+FILE_CHOICE = '''
+Choose a list you want to work with or quit:
+
+f - favorite movies 
+p - past (movies you already saw)
+w - watchlist (movies to see in the future)
+q - quit
+
+>> '''
+
+OPTIONS_INSIDE_FILE = '''
+What would you like to do?
+
+a - add new movie
+d - display all movies
+s - search for particular movie/s
+r - remove a movie
+q - quit
+
+>> '''
+
+
+OPTIONS_SEARCH_BY = '''
+Using which feature would you like to find a movie?
+
+t - title
+y - year
+g - genre
+a - actor
+
+>> '''
