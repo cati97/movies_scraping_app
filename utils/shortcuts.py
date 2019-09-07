9a561281f20fc4e55a26daa49a904c17508b507b
Katarzyna Siwiec <katasiw97@gmail.com>
2019-09-07 20:50:43 +0200

    added two more dicts

---
+++ utils/shortcuts.py
@@ -0,0 +1,18 @@
+file_paths = {
+    'f': '/home/cati/Desktop/movies_scraping/movies_collections/favorite.json',
+    'p': '/home/cati/Desktop/movies_scraping/movies_collections/past.json',
+    'w': '/home/cati/Desktop/movies_scraping/movies_collections/watchlist.json'
+}
+
+list_names = {
+    'f': 'Favorite Movies List',
+    'p': 'Past Movies List',
+    'w': 'Watchlist'
+}
+
+shorts_search_by = {
+    't': 'Title',
+    'y': 'Year',
+    'g': 'Genres',
+    'a': 'Actors'
+}
